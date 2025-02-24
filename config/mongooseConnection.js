2c4221df5d20c80e370f0f73d334b990f429e50b
Abhinav Yadav <ironmanabhi@kali>
2025-02-24 23:25:32 +0530

    seperating config files setting up env variables, creating owner routes

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
+const dbgr = require("debug")("development: mongoose");
+const config = require("config");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/ecomm")
+  .connect(`${config.get(MONGODB_URI)}/ecom`)
   .then(() => {
-    console.log("Connected");
+    dbgr("Connected");
   })
   .catch((err) => {
-    console.log(err);
+    dbgr(err);
   });
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner");
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    let owner = await ownerModel.find();
+    if (owner.lenght > 0)
+      return res
+        .status(504)
+        .send("You dont have permission to create new owner");
+
+    let { fullName, email, password } = req.body;
+    let createdOwner = await ownerModel.create({
+      fullName,
+      email,
+      password,
+    });
+    res.status(200).send(createdOwner);
+  });
+}
 
 router.get("/", (req, res) => {
   res.status(200).send("hello");
